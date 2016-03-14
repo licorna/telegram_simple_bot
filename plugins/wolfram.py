75b6c061d2314a5db116ffe4711a7f27aff327de
Rodrigo Valin <valin@groupon.com>
2016-03-13 21:54:18 -0300

    No format for equations returned from wolf.

--- plugins/wolfram.py
+++ plugins/wolfram.py
@@ -2,9 +2,13 @@ from TelegramBotAPI.types import sendMessage
 from TelegramBot.plugin import BotPlugin
 import asyncio
 import wolframalpha
+from config import WOLFRAM_APPID
 
 APPNAME='Malo Bot'
-WOLFRAM_APPID='<your wolfram appid>'
+
+TITLE_TEMPLATE = '*{title}*\n'
+CODE_TEMPLATE = '```{text}```'
+TEXT_TEMPLATE = '{text}'
 
 class Wolfram(BotPlugin):
 
@@ -50,8 +54,25 @@ def pod_repr(pod):
     'Returns a "line" from the pod, it will be title and text...'
     if not pod.text:
         return
-    return template.format(title=pod.title, text=pod.text)
+    text = (CODE_TEMPLATE if is_no_format(pod.title) else TEXT_TEMPLATE).format(
+        text=pod.text)
 
-template = '''*{title}*
-{text}
-'''
+    return TITLE_TEMPLATE.format(title=pod.title) + text
+
+def is_no_format(title):
+    words = (
+        'Derivative',
+        'Indefinite integral',
+        'Identities',
+        'Global maxima',
+        'Global minima',
+        'Alternative representations',
+        'Series representations',
+        'Integral representations',
+        'Definite integral over a half-period',
+        'Definite integral mean square',
+        'Alternate form',
+        'Roots',
+        'Properties as a real function',
+        'Series expansion at x=0')
+    return title in words
